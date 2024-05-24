99773d2aeaca3f8798e61a07e9f60b18f642088e
lscantillo <luissebastiancantillo@gmail.com>
2024-05-24 16:20:59 -0500

    CHORE: rename main file

---
+++ price_calculator.rb
@@ -0,0 +1,14 @@
+# $ ruby price_calculator.rb
+# Please enter all the items purchased separated by a comma
+# milk,milk, bread,banana,bread,bread,bread,milk,apple
+
+# Item     Quantity      Price
+# --------------------------------------
+# Milk      3            $8.97
+# Bread     4            $8.17
+# Apple     1            $0.89
+# Banana    1            $0.99
+
+# Total price : $19.02
+# You saved $3.45 today.
+
